63b920c25801e6a17233208c5e467a4c921413ed
anuj1995 <43285981+anuj1995@users.noreply.github.com>
2019-03-02 18:37:44 -0500

    made changes for the large dataset

--- python/src/preprocess.py
+++ python/src/preprocess.py
@@ -22,12 +22,18 @@ def get_data(path, size ,train=True, small=True):
     url2 = path + "/test.txt"
     test_text_file_list = requests.get(url2).text
     test_text_file_list = test_text_file_list.split()
+    if small:
+        num_of_train_images = 211
+        num_of_test_images = 114
+    else:
+        num_of_train_images = 21100
+        num_of_test_images = 11400
     if train:
-        X = np.zeros((211, size[0], size[1], 1), dtype=np.float32)
-        y = np.zeros((211, size[0], size[1], 1), dtype=np.float32)
+        X = np.zeros((num_of_train_images, size[0], size[1], 1), dtype=np.float32)
+        y = np.zeros((num_of_train_images, size[0], size[1], 1), dtype=np.float32)
         list = train_text_file_list
     else:
-        X = np.zeros((114, im_height, im_width, 1), dtype=np.float32)
+        X = np.zeros((num_of_test_images, im_height, im_width, 1), dtype=np.float32)
         list = test_text_file_list
     print('Getting and resizing images ... ')
     i = 0
@@ -37,9 +43,6 @@ def get_data(path, size ,train=True, small=True):
         url3 = path+"/data/"+filename+".tar"
         response_0 = requests.get(url3)
         tar = tarfile.open(mode= "r:*", fileobj = BytesIO(response_0.content))
-        if train:
-            url4 = path + "/masks/" + filename+".png"
-            response = requests.get(url4)
         for member in tar.getnames():
             image = np.asarray(bytearray(tar.extractfile(member).read()), dtype="uint8")
             image = cv2.imdecode(image, cv2.IMREAD_GRAYSCALE)
@@ -47,9 +50,11 @@ def get_data(path, size ,train=True, small=True):
             X[i, ..., 0] = image / 255
             if train:
                 # path for masks
+                url4 = path + "/masks/" + filename+".png"
+                response = requests.get(url4)
                 mask = np.asarray(bytearray(response.content), dtype="uint8")
                 mask = cv2.imdecode(mask, cv2.IMREAD_GRAYSCALE)
-                mask = cv2.resize(mask,(size[0],size[1))
+                mask = cv2.resize(mask,(size[0],size[1]))
                 mask[mask==0]=0
                 mask[mask==1]=0
                 mask[mask==2]=1
@@ -63,4 +68,4 @@ def get_data(path, size ,train=True, small=True):
     else:
         return X
 
-X_train, y_train = get_data("https://storage.googleapis.com/uga-dsp/project2",(256,256) ,train=True, small=True)
+X_train, y_train = get_data("https://storage.googleapis.com/uga-dsp/project2",(512,512) ,train=True, small=False)
