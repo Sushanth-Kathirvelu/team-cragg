8624dcf87f66c64154112e4d6b2977b87b44b0a8
anuj1995 <43285981+anuj1995@users.noreply.github.com>
2019-03-05 23:37:36 -0500

    added resize

---
+++ src/PreProcess/Resize.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2
+import os
+
+class Resize:
+    def resize(path,size,imagePath, savePath):
+        """function to resize the images
+        path -- path of the train.text or file.text
+        size -- tuple for the new image dimensions
+        imagePath -- path for images or masks
+        savePath -- path forsaving the images"""
+        if not os.path.exist(savePath):
+            os.mkdir(savePath)
+        file = open(path)
+        for hashNames in file:
+            hashNames = hashNames.split()[0]
+            img = cv2.imread(imagePath + hashNames +".png")
+            img = cv2.resize(img,(size[1],size[0]))
+            cv2.imwrite(save_path + "/"+ hashNames +".png")
